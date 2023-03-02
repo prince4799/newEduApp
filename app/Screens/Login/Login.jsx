483b89c1c27180ac5f6d3a241424b58a400b4838
Prince <prince.varma@kreatetechnologies.com>
2023-03-02 18:38:13 +0530

    ts for context api

--- app/Screens/Login/Login.jsx
+++ app/Screens/Login/Login.jsx
@@ -31,9 +31,9 @@ import Animated from "react-native-reanimated";
 import { COLORS } from '../../Constants/Colors';
 // import * as FirebaseAuth from "../../Firebase/FirebaseAuth"
 // import { SignInUser } from '../../Firebase/FirebaseAuth';
-import {SignInUser,CreateNewUser} from '../../Firebase/FirebaseAuth'
+import { SignInUser, CreateNewUser } from '../../Firebase/FirebaseAuth'
 import * as firebaseDB from '../../Firebase/FirebaseDB';
-const Login = ({navigation}) => {
+const Login = ({ navigation }) => {
 
   const { height, width, scale, fontScale } = useWindowDimensions()
   const [email, setEmail] = useState('')
@@ -55,82 +55,106 @@ const Login = ({navigation}) => {
   };
 
 
+
   return (
     <SafeAreaView style={styles.container}>
-            <ImageBackground style={{height:height,width:width,justifyContent:'center',alignItems:'center',}} resizeMode='cover' source={require("../../Assets/Images/gradient_bg.png")} >
-      <Animated.View style={{flipToFrontStyle}}>
-      <ImageBackground style={{ ...styles.form(height, width),}} source={require("../../Assets/Images/half_bg.png")} >
+      <ImageBackground style={{ height: height, width: width, justifyContent: 'center', alignItems: 'center', }} resizeMode='cover' source={require("../../Assets/Images/gradient_bg.png")} >
+        <Animated.View style={{ flipToFrontStyle }}>
+          <ImageBackground style={{ ...styles.form(height, width), }} source={require("../../Assets/Images/half_bg.png")} >
 
-      <View style={{ ...styles.form(height, width), }}>
-        <ScrollView style={{ height: (height / 10) * 8, }}>
+            <View style={{ ...styles.form(height, width), }}>
+              <ScrollView style={{ height: (height / 10) * 8, }}>
 
-          <View style={{ alignSelf: 'center' }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 24, top:10, color: COLORS.Font }}>LOGIN</Text>
-            <View style={{ height: '20%', width: '25%', backgroundColor: '#fff', top: 0, margin: 10 }} />
-            <Text>APP LOGO</Text>
-          </View>
+                <View style={{ alignSelf: 'center' }}>
+                  <Text style={{ fontWeight: 'bold', fontSize: 24, top: 10, color: COLORS.Font }}>LOGIN</Text>
+                  <View style={{ height: '20%', width: '25%', backgroundColor: '#fff', top: 0, margin: 10 }} />
+                  <Text>APP LOGO</Text>
+                </View>
 
-          <KeyboardAvoidingView style={{ height: height > width ? portrait : landscape, width: '90%', alignSelf: 'center', marginBottom: 80, }} behavior='padding' >
-            <View style={{ ...styles.inputContainer }} >
-              <Image style={styles.inputImg} source={require('../../Assets/Images/mail.png')} />
-              <TextInput style={styles.inputs}
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-                placeholder='Enter Your Mail' />
+                <KeyboardAvoidingView style={{ height: height > width ? portrait : landscape, width: '90%', alignSelf: 'center', marginBottom: 80, }} behavior='padding' >
+                  <View style={{ ...styles.inputContainer }} >
+                    <Image style={styles.inputImg} source={require('../../Assets/Images/mail.png')} />
+                    <TextInput style={styles.inputs}
+                      value={email}
+                      onChangeText={(text) => setEmail(text)}
+                      placeholder='Enter Your Mail' />
+                  </View>
+                  <View style={styles.inputContainer}>
+                    <Image style={styles.inputImg} source={require('../../Assets/Images/padlock.png')} />
+
+                    <TextInput style={styles.inputs}
+                      value={password}
+                      onChangeText={(text) => setPassword(text)}
+                      placeholder='Enter Your Password' />
+                    <Image style={{ ...styles.inputImg, }} source={require('../../Assets/Images/eye.png')} />
+                  </View>
+
+                  <View style={{ bottom: 0, top: '40%' }}>
+
+                    <TouchableOpacity
+                      // onPress={()=>SignInUser({email,password})}
+                      onPress={() => navigation.navigate('BottomTab')}
+
+                      // onPress={()=>firebaseDB.FirebaseDBPush({email,password})}
+                      // onPress={()=>firebaseDB.FirebaseDBRead()}
+
+                      style={{
+                        ...styles.button,
+                        width: (width / 10) * 6,
+                      }}>
+                      <Text style={styles.buttonText}>LOGIN</Text>
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity
+                      style={{
+                        ...styles.button,
+                        width: (width / 10) * 6,
+                        alignItems: 'center',
+                      }}
+                      onPress={() => {
+                        try {
+
+                          fetch('http://192.168.101.57:5500/auth/google',
+                            {
+                              method: 'POST',
+                              headers:{
+                                "Content-Type":"application/json"
+                              },
+                            //   body: JSON.stringify({
+                            //     "userid":"manav",
+                            //     "password":"1234",
+                            //     // "email":"manav@mail.com",
+                            //     // "contact":9988556
+                            // })
+                            })
+                            .then((res) => res.json())
+                            .then(res => console.log(JSON.stringify(res)))
+                        } catch (err) {
+                          console.log("errr in hitting api", err)
+                        }
+                      }}
+                    >
+                      <Text style={styles.buttonText}>SignIn with google</Text>
+                      <Image
+                        style={{
+                          ...styles.inputImg,
+                          height: 30,
+                          width: 30,
+                          tintColor: null
+                        }}
+                        source={require('../../Assets/Images/google.png')} />
+                    </TouchableOpacity>
+                  </View>
+
+                </KeyboardAvoidingView>
+              </ScrollView>
+              {/* <Text onPress={()=>flipToFrontStyle()} style={styles.link}>Create New Account.</Text> */}
+
             </View>
-            <View style={styles.inputContainer}>
-              <Image style={styles.inputImg} source={require('../../Assets/Images/padlock.png')} />
-
-              <TextInput style={styles.inputs}
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-                placeholder='Enter Your Password' />
-              <Image style={{ ...styles.inputImg, }} source={require('../../Assets/Images/eye.png')} />
-            </View>
-
-            <View style={{ bottom: 0, top: '40%' }}>
-
-              <TouchableOpacity
-              // onPress={()=>SignInUser({email,password})}
-                onPress={()=> navigation.navigate('BottomTab')}
-
-              // onPress={()=>firebaseDB.FirebaseDBPush({email,password})}
-              // onPress={()=>firebaseDB.FirebaseDBRead()}
-
-              style={{
-                ...styles.button,
-                width: (width / 10) * 6,
-              }}>
-                <Text style={styles.buttonText}>LOGIN</Text>
-              </TouchableOpacity>
-
-            
-              <TouchableOpacity
-                style={{
-                  ...styles.button,
-                  width: (width / 10) * 6,
-                  alignItems: 'center',
-                }}>
-                <Text style={styles.buttonText}>SignIn with google</Text>
-                <Image
-                  style={{
-                    ...styles.inputImg,
-                    height: 30,
-                    width: 30,
-                    tintColor: null
-                  }}
-                  source={require('../../Assets/Images/google.png')} />
-              </TouchableOpacity>
-            </View>
-            
-          </KeyboardAvoidingView>
-        </ScrollView>
-          {/* <Text onPress={()=>flipToFrontStyle()} style={styles.link}>Create New Account.</Text> */}
-
-      </View>
-                  <Text onPress={()=> navigation.navigate('SignUp')} style={[styles.link]}>Create  An Account</Text>
-      </ImageBackground>
-      </Animated.View>
+            <Text onPress={() => navigation.navigate('SignUp')} style={[styles.link]}>Create  An Account</Text>
+          </ImageBackground>
+        </Animated.View>
       </ImageBackground>
 
     </SafeAreaView>
@@ -194,12 +218,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: COLORS.ButtonText
   },
-  link:{
-    color:COLORS.Links,
-    textDecorationLine:'underline',
-    alignSelf:'center',
+  link: {
+    color: COLORS.Links,
+    textDecorationLine: 'underline',
+    alignSelf: 'center',
     // backgroundColor:'red',
-    bottom:'5%'
+    bottom: '5%'
   }
 });
 
